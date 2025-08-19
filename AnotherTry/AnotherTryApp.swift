e82a22e9a1c687ad610b77dfa4a170b6f3d15867
John3771 <sokolnumber1@gmail.com>
2025-08-20 00:56:06 +0300

    Initial Commit

---
+++ AnotherTry/AnotherTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherTryApp.swift
+//  AnotherTry
+//
+//  Created by Gleb on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnotherTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
